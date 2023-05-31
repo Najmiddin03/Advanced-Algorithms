ae28fd098f59b9e54c0fa67a7c1d9a5fcbc4da66
Najmiddin03 <nazarmatovn03@gmail.com>
2023-05-31 03:38:23 +0200

    wildcards

---
+++ AssignmentOnePartTwo/src/Wildcards.java
@@ -0,0 +1,73 @@
+public class Wildcards {
+
+    public static boolean bruteForce(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                char textChar = text.charAt(i + j);
+                char patternChar = pattern.charAt(j);
+                if (patternChar != '?' && patternChar != '*' && patternChar != textChar) {
+                    break;
+                }
+            }
+            if (j == m) {
+                return true; // Match found
+            }
+        }
+        return false; // No match found
+    }
+
+    public static boolean sunday(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+        int i = 0;
+
+        while (i <= n - m) {
+            int j;
+            for (j = 0; j < m; j++) {
+                char textChar = text.charAt(i + j);
+                char patternChar = pattern.charAt(j);
+                if (patternChar != '?' && patternChar != '*' && patternChar != textChar) {
+                    // Calculate the shift for the next character in text
+                    int shift = calculateShift(text, i + m, patternChar);
+                    i += shift;
+                    break;
+                }
+            }
+            if (j == m) {
+                return true; // Match found
+            }
+        }
+        return false; // No match found
+    }
+
+    private static int calculateShift(String text, int index, char target) {
+        int n = text.length();
+        if (index >= n) {
+            return 1; // Shift by 1 if target character is not found
+        }
+
+        char nextChar = text.charAt(index);
+        if (nextChar == target) {
+            return 1; // Shift by 1 if target character is found
+        } else if (nextChar == '?' || nextChar == '*') {
+            return 1 + calculateShift(text, index + 1, target); // Recursively calculate the shift
+        } else {
+            return calculateShift(text, index + 1, target); // Recursively calculate the shift
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "Hel?o World!";
+        String pattern = "He*?o Wo*";
+
+        boolean bruteForceMatch = bruteForce(text, pattern);
+        boolean sundayMatch = sunday(text, pattern);
+
+        System.out.println("Brute-force match: " + bruteForceMatch);
+        System.out.println("Sunday match: " + sundayMatch);
+    }
+}
--- assignmentOnePartOne/src/B.java
+++ assignmentOnePartOne/src/B.java
@@ -11,39 +11,43 @@ public class B {
             text += scanner.nextLine();
         }
         String pattern = "algorithm";
-        long start, end, sunday, gusfield, kmp, rabin;
+        long start, end, sunday = 0, gusfield = 0, kmp = 0, rabin = 0;
 
+        for (int i = 0; i < 1000; i++) {
+
+            // Test Sunday
+            start = System.nanoTime();
+            PatternMatchingAlgorithms.sunday(text, pattern);
+            end = System.nanoTime();
+            sunday += end - start;
+
+            // Test Gusfield
+            start = System.nanoTime();
+            PatternMatchingAlgorithms.gusfieldZ(text, pattern);
+            end = System.nanoTime();
+            gusfield += end - start;
+
+            // Test KMP
+            start = System.nanoTime();
+            PatternMatchingAlgorithms.kmp(text, pattern);
+            end = System.nanoTime();
+            kmp += end - start;
+
+            // Test Rabin-Karp
+            start = System.nanoTime();
+            PatternMatchingAlgorithms.rabinKarp(text, pattern);
+            end = System.nanoTime();
+            rabin += end - start;
+        }
+        sunday /= 1000;
+        gusfield /= 1000;
+        rabin /= 1000;
+        kmp /= 1000;
         // Sunday vs. Gusfield Z
-        // Test Sunday
-        start = System.nanoTime();
-        PatternMatchingAlgorithms.sunday(text, pattern);
-        end = System.nanoTime();
-        sunday = end - start;
-
-        // Test Gusfield
-        start = System.nanoTime();
-        PatternMatchingAlgorithms.gusfieldZ(text, pattern);
-        end = System.nanoTime();
-        gusfield = end - start;
-
-        System.out.println("Sunday is " + gusfield / sunday + " times as fast as GusfieldZ");
-
-
+        System.out.println("Sunday is " + (double) gusfield / sunday + " times as fast as GusfieldZ");
         // KMP vs. Rabin-Karp
-        // Test KMP
-        start = System.nanoTime();
-        PatternMatchingAlgorithms.kmp(text, pattern);
-        end = System.nanoTime();
-        kmp = end - start;
-
-        // Test Rabin-Karp
-        start = System.nanoTime();
-        PatternMatchingAlgorithms.rabinKarp(text, pattern);
-        end = System.nanoTime();
-        rabin = end - start;
-
-        System.out.println(rabin+" "+kmp);
-        System.out.println("KMP is " + (double)rabin / kmp + " times as fast as Rabin-Karp");
-
+        System.out.println("KMP is " + (double) rabin / kmp + " times as fast as Rabin-Karp");
+        // Rabin-Karp vs. Sunday
+        System.out.println("Rabin-Karp is " + (double) sunday / rabin + " times as fast as Sunday");
     }
 }
