b9db1b9aea4dfdea7d60f9395b938b96bb831ca4
Najmiddin03 <nazarmatovn03@gmail.com>
2023-06-07 13:25:16 +0200

    wildcards fixed

--- AssignmentOnePartTwo/src/Wildcards.java
+++ AssignmentOnePartTwo/src/Wildcards.java
@@ -6,19 +6,38 @@ public class Wildcards {
     public static boolean bruteForce(String text, String pattern) {
         int n = text.length();
         int m = pattern.length();
-
-        for (int i = 0; i <= n - m; i++) {
-            int j;
-            for (j = 0; j < m; j++) {
+        int i = 0;
+        while (i <= n - m) {
+            int j = 0;
+            int matchIndex = -1;
+            while (j < m) {
                 char textChar = text.charAt(i + j);
                 char patternChar = pattern.charAt(j);
                 if (patternChar != '?' && patternChar != '*' && patternChar != textChar) {
                     break;
                 }
+                if (patternChar == '*') {
+                    matchIndex = j;
+                }
+                j++;
             }
             if (j == m) {
                 return true;
             }
+            if (matchIndex != -1) {
+                char nextPatternChar = pattern.charAt(matchIndex + 1);
+                int k = i + matchIndex;
+                while (k <= n - m + 1) {
+                    if (bruteForce(text.substring(k), pattern.substring(matchIndex + 1))) {
+                        return true;
+                    }
+                    if (k == n - 1 || (nextPatternChar != '?' && nextPatternChar != '*' && nextPatternChar != text.charAt(k + 1))) {
+                        break;
+                    }
+                    k++;
+                }
+            }
+            i++;
         }
         return false;
     }
@@ -27,39 +46,48 @@ public class Wildcards {
         int n = text.length();
         int m = pattern.length();
         int i = 0;
-
         while (i <= n - m) {
-            int j;
-            for (j = 0; j < m; j++) {
+            int j = 0;
+            while (j < m) {
                 char textChar = text.charAt(i + j);
                 char patternChar = pattern.charAt(j);
                 if (patternChar != '?' && patternChar != '*' && patternChar != textChar) {
-                    int shift = calculateShift(text, i + m, patternChar);
-                    i += shift;
                     break;
                 }
+                if (patternChar == '*') {
+                    if (j == m - 1) {
+                        return true;
+                    }
+                    char nextPatternChar = pattern.charAt(j + 1);
+                    int k = i + j;
+                    while (k <= n - m + 1) {
+                        if (sunday(text.substring(k), pattern.substring(j + 1))) {
+                            return true;
+                        }
+                        if (k == n - 1 || (nextPatternChar != '?' && nextPatternChar != '*' && nextPatternChar != text.charAt(k + 1))) {
+                            break;
+                        }
+                        k++;
+                    }
+                    break;
+                }
+
+                j++;
             }
             if (j == m) {
                 return true;
             }
+            int shift = 1;
+            if (i + m < n) {
+                char nextChar = text.charAt(i + m);
+                int index = pattern.indexOf(nextChar);
+                if (index != -1 && index < m - 1) {
+                    shift = m - index - 1;
+                }
+            }
+            i += shift;
         }
         return false;
-    }
-
-    private static int calculateShift(String text, int index, char target) {
-        int n = text.length();
-        if (index >= n) {
-            return 1;
-        }
-
-        char nextChar = text.charAt(index);
-        if (nextChar == target) {
-            return 1;
-        } else if (nextChar == '?' || nextChar == '*') {
-            return 1 + calculateShift(text, index + 1, target);
-        } else {
-            return calculateShift(text, index + 1, target);
-        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -69,9 +97,10 @@ public class Wildcards {
         while (scanner.hasNextLine()) {
             text += scanner.nextLine();
         }
-        String pattern1 = "algo*?thm";
-        String pattern2 = "m*?k";
-        String pattern3 = "De*?line";
+
+        String pattern1 = "a*or?thm";
+        String pattern2 = "ma?k";
+        String pattern3 = "De*line";
         String pattern4 = "hel*o";
 
         System.out.println("Brute Force:");
@@ -85,6 +114,5 @@ public class Wildcards {
         System.out.println(pattern2 + ": " + sunday(text, pattern2));
         System.out.println(pattern3 + ": " + sunday(text, pattern3));
         System.out.println(pattern4 + ": " + sunday(text, pattern4));
-
     }
 }
